a18fb5f5851e6f71e4b04174b0aed8cacef89301
Rinat <Sandyans@yandex.ru>
2022-11-20 21:58:13 +0500

    2. Делаю dll для работы и просмотра

---
+++ DaDataMakeDLL/ConsoleDaData/ConsoleDaData/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DaDataDLL;
+using static DaDataDLL.DaData;
+
+namespace ConsoleDaData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaData daData = new DaData("e2b850d5b2bf12b272b5e2ba475239a45aa3b6c6");
+           List<Organisation> organisation =  daData.QueryByInn("7813252159"); //Проверяю по ИНН Гознак
+
+            if (organisation != null)
+            {
+                foreach (Organisation o in organisation)
+                {
+                    Console.WriteLine(o.name_organisation);
+                    Console.WriteLine(o.addres);                    
+                    Console.WriteLine(o.inn);
+                    Console.WriteLine(o.kpp);
+                    Console.WriteLine(o.ogrn);
+                    Console.WriteLine(o.opf_short);
+                    Console.WriteLine(o.status_organisation);
+                    Console.WriteLine(o.type);
+                    Console.WriteLine(o.registration_date);
+                    Console.WriteLine(o.actual_data_organisation);
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DaDataMakeDLL/DaDataDLL/DaDataDLL/DaData.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DaData.Client;
+
+namespace DaDataDLL
+{
+    public class DaData
+    {   
+        SuggestClient api;
+
+        public DaData(string token)
+        {          
+            var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs"; // может менятся в последний раз менялся в 2020 году
+            this.api = new SuggestClient(token, url);            
+        }
+        /// <summary>
+        /// получаеам по INN
+        /// </summary>
+        /// <param name="INN"></param>
+        public List<Organisation> QueryByInn(string INN)
+        {
+            try
+            {
+                List<Organisation> organisations = new List<Organisation>();
+                var response = api.QueryParty(INN);
+                foreach (var organisation in response.suggestions)
+                {
+                    
+                    DateTime reg_date;
+                    if (organisation.data.state.registration_date == null)
+                        reg_date = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                    else
+                        reg_date = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddMilliseconds(long.Parse(organisation.data.state.registration_date));
+                    DateTime actualDate;
+                    if (organisation.data.state.actuality_date == null)
+                        actualDate = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                    else
+                        actualDate = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddMilliseconds(long.Parse(organisation.data.state.actuality_date));
+                    string status;
+                    if (organisation.data.state.status != null)
+                    {
+                        status = organisation.data.state.status.ToString();
+                    } else
+                    {
+                        status = "unkom";
+                    }
+
+                    organisations.Add(new Organisation
+                    {
+                        name_organisation = organisation.value,
+                        addres = organisation.data.address.value,
+                        inn = organisation.data.inn,
+                        kpp = organisation.data.kpp,
+                        ogrn = organisation.data.ogrn,
+                        type = organisation.data.type.ToString(),
+                        okpo = organisation.data.okpo,
+                        opf_short = organisation.data.opf.@short,
+                        status_organisation = status,
+                        registration_date = reg_date,
+                        actual_data_organisation = actualDate
+                    });
+                }
+
+                return organisations;
+            }
+            catch(Exception ex)
+            {
+               string message = ex.Message;
+                return null;
+            }
+        }
+        public class Organisation
+        {
+            public string name_organisation { get; set; } = null;
+            public string addres { get; set; } = null;
+            public string inn { get; set; } = null;
+           public string kpp { get; set; } = null;
+            public string ogrn { get; set; } = null;
+            public string type { get; set; } = null;
+            public string okpo { get; set; } = null;
+            public string opf_short { get; set; } = null;
+            public string status_organisation { get; set; } = null;
+            public DateTime registration_date { get; set; }
+            public DateTime actual_data_organisation { get; set; }
+        }
+    }
+}
