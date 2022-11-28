e0cdec6a8cbb3e3813051eb2b6a71f4a33d524f0
Rinat <Sandyans@yandex.ru>
2022-11-28 23:26:49 +0500

    4. Добавил полную реализацию получения данных о компании через DaData

--- DaDataMakeDLL/ConsoleDaData/ConsoleDaData/Program.cs
+++ DaDataMakeDLL/ConsoleDaData/ConsoleDaData/Program.cs
@@ -12,15 +12,25 @@ namespace ConsoleDaData
     {
         static void Main(string[] args)
         {
+            string inn = "5904002096";//"7813252159";
             DaData daData = new DaData("e2b850d5b2bf12b272b5e2ba475239a45aa3b6c6");
-           List<Organisation> organisation =  daData.QueryByInn("7813252159"); //Проверяю по ИНН Гознак
+            // shortInformation(daData, inn);
+             getlongInformation(daData, inn);
+    
+            Console.ReadKey();
+        }
+
+        #region короткая информация о компании
+        public static void shortInformation(DaData daData, string inn) 
+        {
+            List<Organisation> organisation = daData.QueryByInn("7813252159"); //Проверяю по ИНН Гознак
 
             if (organisation != null)
             {
                 foreach (Organisation o in organisation)
                 {
                     Console.WriteLine(o.name_organisation);
-                    Console.WriteLine(o.addres);                    
+                    Console.WriteLine(o.addres);
                     Console.WriteLine(o.inn);
                     Console.WriteLine(o.kpp);
                     Console.WriteLine(o.ogrn);
@@ -28,11 +38,47 @@ namespace ConsoleDaData
                     Console.WriteLine(o.status_organisation);
                     Console.WriteLine(o.type);
                     Console.WriteLine(o.registration_date);
-                    Console.WriteLine(o.actual_data_organisation);
+                    Console.WriteLine(o.actual_data_organisation);                    
                     Console.WriteLine();
                 }
             }
-            Console.ReadKey();
         }
+        #endregion
+
+        public static void getlongInformation(DaData daData, string inn) 
+        {
+         List<EgrulResponse> egrulResponses = daData.QueryByInnAsEgulResponse(inn);
+            if (egrulResponses != null) 
+            {
+                foreach (EgrulResponse e in egrulResponses) 
+                {                    
+                    Console.WriteLine("Full name: {0}", e.fullName);
+                    Console.WriteLine("Short_naem: {0}",    e.shortName);
+                    Console.WriteLine("Opf: {0}",e.opf_short);
+                    Console.WriteLine("INN: {0}",   e.inn);
+                    Console.WriteLine("KPP: {0}",   e.kpp);
+                    Console.WriteLine("OGRN: {0}",  e.ogrn);
+                    Console.WriteLine("OGRNIP: {0}",    e.ogrnip);
+                    Console.WriteLine("EMAIL: {0}", e.email);
+                    Console.WriteLine("CodeOkved: {0}", e.codeOkved);
+                    Console.WriteLine("Full adres: {0}", e.fullAddress);
+                    Console.WriteLine("INDEX: {0}", e.index);
+                    Console.WriteLine("Region: {0}", e.region);
+                    Console.WriteLine("CodeRegion: {0}",    e.codeRegion);
+                    Console.WriteLine("Район: {0}", e.rayon);
+                    Console.WriteLine("Город: {0}", e.city);
+                    Console.WriteLine("Улица: {0}", e.street);
+                    Console.WriteLine("Дом: {0}",   e.house);
+                    Console.WriteLine("Квартира: {0}",  e.flat);
+                    Console.WriteLine("Страна: {0}",    e.country);
+                    Console.WriteLine("OKATO: {0}", e.okato);
+                    Console.WriteLine("OKTMO: {0}", e.oktmo);
+                    Console.WriteLine("KLADR: {0}", e.kladr);
+                    Console.WriteLine(" ");
+                }
+            }
+
+        }
+
     }
 }
--- DaDataMakeDLL/DaDataDLL/DaDataDLL/DaData.cs
+++ DaDataMakeDLL/DaDataDLL/DaDataDLL/DaData.cs
@@ -10,7 +10,7 @@ namespace DaDataDLL
     public class DaData
     {   
         SuggestClient api;
-
+        string Error = null;
         public DaData(string token)
         {          
             var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs"; // может менятся в последний раз менялся в 2020 году
@@ -72,19 +72,124 @@ namespace DaDataDLL
                 return null;
             }
         }
-        public class Organisation
+
+        //получить в том порядке как из Egrul в GZAgr
+        public List<EgrulResponse> QueryByInnAsEgulResponse(string INN)
         {
-            public string name_organisation { get; set; } = null;
-            public string addres { get; set; } = null;
-            public string inn { get; set; } = null;
-           public string kpp { get; set; } = null;
-            public string ogrn { get; set; } = null;
-            public string type { get; set; } = null;
-            public string okpo { get; set; } = null;
-            public string opf_short { get; set; } = null;
-            public string status_organisation { get; set; } = null;
-            public DateTime registration_date { get; set; }
-            public DateTime actual_data_organisation { get; set; }
+            List<EgrulResponse> egrulResponses = new List<EgrulResponse>();
+            try
+            {
+                var response = api.QueryParty(INN);
+                foreach (var organisation in response.suggestions)
+                {
+
+                    DateTime reg_date;
+                    if (organisation.data.state.registration_date == null)
+                        reg_date = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                    else
+                        reg_date = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddMilliseconds(long.Parse(organisation.data.state.registration_date));
+                    DateTime actualDate;
+                    if (organisation.data.state.actuality_date == null)
+                        actualDate = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                    else
+                        actualDate = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddMilliseconds(long.Parse(organisation.data.state.actuality_date));
+                    string status;
+                    if (organisation.data.state.status != null)
+                    {
+                        status = organisation.data.state.status.ToString();
+                    }
+                    else
+                    {
+                        status = "unkom";
+                    }
+                    egrulResponses.Add(new EgrulResponse()
+                    { // записываем в том порядке как в GZAgr
+                        fullName = organisation.data.name.full, // полное наименование 
+                        shortName = organisation.data.name.@short, // короткое наименование
+                        inn = organisation.data.inn,
+                        kpp = organisation.data.kpp,
+                        ogrn = organisation.data.ogrn,
+                        opf_short = organisation.data.okpo,
+                        ogrnip = "",
+                        email = organisation.data.management.post,
+                        codeOkved = organisation.data.okved_type,
+                        fullAddress = organisation.data.address.value,
+                        index = organisation.data.address.data.postal_code,
+                        region = organisation.data.address.data.region,
+                        codeRegion = organisation.data.address.data.region_fias_id,
+                        rayon = organisation.data.address.data.city_area,
+                        city = organisation.data.address.data.city,
+                        street = organisation.data.address.data.street,
+                        house = organisation.data.address.data.house,
+                        flat = organisation.data.address.data.flat,
+                        country = organisation.data.address.data.country,
+                        okato = organisation.data.address.data.okato,
+                        oktmo = organisation.data.address.data.oktmo,
+                        kladr = organisation.data.address.data.kladr_id,
+                        status_organisation = status,
+                        registration_date = reg_date,
+                        actual_data_organisation = actualDate 
+                    }) ; 
+                }
+                return egrulResponses;
+            }
+            catch (Exception ex)
+            {
+                this.Error = ex.Message;
+                return null;
+            }
+        }
+
+        public string getPartyQuery(string Party) 
+        {
+            string index = "";
+            var response = api.QueryParty(Party);
+            foreach (var r in response.suggestions) 
+            {
+               index = r.data.management.post;            
+            }
+            return index;
         }
     }
+
+    public class Organisation
+    {
+        public string name_organisation { get; set; } = null;
+        public string addres { get; set; } = null;
+        public string inn { get; set; } = null;
+        public string kpp { get; set; } = null;
+        public string ogrn { get; set; } = null;
+        public string type { get; set; } = null;
+        public string okpo { get; set; } = null;
+        public string opf_short { get; set; } = null;
+        public string status_organisation { get; set; } = null;
+        public DateTime registration_date { get; set; }
+        public DateTime actual_data_organisation { get; set; }
+    }
+
+    #region берем наименование по новому со всеми полями которые присутствуют в GZAGR. 
+    public class EgrulResponse : Organisation
+    {
+        //привести к значению null  в случае если не 
+        public string fullName { get; set; } = null;
+        public string shortName { get; set; } = null;
+        public string ogrnip { get; set; } = null;
+        public string email { get; set; } = null;
+        public string codeOkved { get; set; } = null;
+        public string fullAddress { get; set; } = null;
+        public string index { get; set; } = null;
+        public string region { get; set; } = null;
+        public string codeRegion { get; set; } = null;
+        public string rayon { get; set; } = null;
+        public string city { get; set; } = null;
+        public string street { get; set; } = null;
+        public string house { get; set; } = null;
+        public string flat { get; set; } = null;
+        public string country { get; set; } = null;
+        public string okato { get; set; } = null;
+        public string oktmo { get; set; } = null;
+        public string kladr { get; set; } = null;
+    }
+    #endregion
+
 }
